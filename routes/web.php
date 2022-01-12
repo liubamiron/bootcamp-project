ef35ce63e53aabbe98bf40d150af74fb97a03e8b
liubamiron <liuba.miron@gmail.com>
2022-01-13 00:38:58 +0200

    creating route 'contacts

---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\HouseController;
+use App\Http\Controllers\AppartmentController;
+use App\Http\Controllers\ContactUsController;
+use App\Http\Controllers\RegistrationController;
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\ItemController;
+
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+
+
+
+ Route::get('/registration', function () {return view('registration'); })->name('registration');
+
+ Route::get('/houses', [HouseController::class, 'index'])->name('house');
+
+ Route::get('/appartments', [AppartmentController::class, 'index'])
+ ->name('appartment');
+
+ Route::get('/blog', [BlogController::class, 'index'])->name('blog');
+
+ Route::get('/blog/article/{id}', [ArticleController::class, 'show'])
+ ->name('blogArticle');
+
+
+Route::get('/appartments/item/', [ItemController::class, 'index'])->name('appartmentsItem');
+
+Route::get('/houses/item/', [ItemController::class, 'index'])->name('housesItem');
+
+Route::get('contacts', [ContactUsController::class, 'view'])->name('contactUs');
+
