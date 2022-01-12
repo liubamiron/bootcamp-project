9f349bf21717e575e833529483447b3c95166866
liubamiron <liuba.miron@gmail.com>
2022-01-13 00:32:51 +0200

    crearea contactUs controller

---
+++ controllers/ContactUsController.php
@@ -0,0 +1,9 @@
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+class ContactUsController extends Controller
+
+{
+    public function view(){
+        return view(‘contacts.contactUs’);
+    }
+}
