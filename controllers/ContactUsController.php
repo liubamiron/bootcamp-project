3e15d85e328c34c2cb3836daa472e6d2e3085f22
liubamiron <liuba.miron@gmail.com>
2022-01-13 14:56:22 +0200

    objects from the date

--- controllers/ContactUsController.php
+++ controllers/ContactUsController.php
@@ -1,35 +1,50 @@
 <?php
+
 namespace App\Http\Controllers;
 
-use Illuminate\Contracts\View\View;
 use App\Http\Request\ContactUsRequest;
-use App\Http\Controllers\RedirectResponse;
+// use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Contracts\View\View;
 use Illuminate\Mail\Message;
-use Illuminate\Http\Request;
-
 
 class ContactUsController extends Controller
 {
     public function view(){
-        return view(‘contacts.contactUs’);
+        return view('contacts.contactUs');
     }
+
     public function send(ContactUsRequest $request): RedirectResponse
     {
         // dd($request->validated());
 
         $callback = function (string $test): string {
-            return "{$test} input";
-        };
+             return "{$test} input";
+         };
+
 
         $data = $request->validated();
 
         \Log::debug($callback('test'), $data);
 
-        \Mail::raw('This is test email', function (Message $message) {
-            $message->to('tech@baloon.app');
-        });
-      
-       
+        \Mail::send(
+            'emails.contactUs', 
+            [
+                'email' => $data['email'],
+                'name' => $data['name'],
+                'districts' => $data['districts'],
+                'department' => $data['department'],
+                'messageText' => $data['message'],
+           
+            ],
+            function (Message $message) use ($data) {
+                $message->subject('message from ' . $data['email']);
+                $message->to('tech@baloon.app');
+                $message->from('allrent@baloon.app', 'Allrent mailer');
+        }
+        );
+
         return redirect()->route('contactUs')->withInput($data);
+        
     }
 }
