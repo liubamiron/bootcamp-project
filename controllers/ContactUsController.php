bca2f846c254e372aa901c487b342e4db5c1b935
liubamiron <liuba.miron@gmail.com>
2022-01-13 08:39:24 +0200

    Redirecting to named route, using redirect helper

--- controllers/ContactUsController.php
+++ controllers/ContactUsController.php
@@ -1,9 +1,14 @@
+<?php
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
+use App\Http\Controllers\RedirectResponse;
 class ContactUsController extends Controller
-
 {
     public function view(){
         return view(‘contacts.contactUs’);
+    }
+    public function send(): RedirectResponse
+    {
+        return redirect()->route(‘contactUs’);
     }
 }
