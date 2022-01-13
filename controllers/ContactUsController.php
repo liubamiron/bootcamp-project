dadf79b00cf92e4f8152e7cca4214f8ee36fe366
liubamiron <liuba.miron@gmail.com>
2022-01-13 12:24:57 +0200

    add use App\Http\Request\ContactUsrequest;

--- controllers/ContactUsController.php
+++ controllers/ContactUsController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
+use App\Http\Request\ContactUsRequest;
 use App\Http\Controllers\RedirectResponse;
 class ContactUsController extends Controller
 {
