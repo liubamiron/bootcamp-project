17f892b3e069f2a4a5208ddea6cec8830a49c47f
liubamiron <liuba.miron@gmail.com>
2022-01-14 18:39:35 +0200

    keeping in log -  email send

--- app/Services/ContactUsMailer.php
+++ app/Services/ContactUsMailer.php
@@ -3,14 +3,21 @@
 namespace App\Services;
 
 use Illuminate\Mail\Mailer;
+use Illuminate\Mail\Message;
+use Psr\Log\LoggerInterface;
 
 class ContactUsMailer
 {
     private  Mailer $infrastructureMailer;
+    private LoggerInterface $logger;
 
-    public function  __construct(Mailer $infrastructureMailer)
+   
+
+
+    public function  __construct(Mailer $infrastructureMailer, LoggerInterface $logger)
     {
         $this->infrastructureMailer = $infrastructureMailer;
+        $this->logger = $logger;
     }
 
     public function send(array $data): void
@@ -32,6 +39,7 @@ class ContactUsMailer
                 $message->from('allrent@gmail.com', 'AllRent mailer');
             }
         );
-    }
 
+        $this->logger->info('Contact Us mail send to contact_allrent@gmail.com');
+    }
 }
