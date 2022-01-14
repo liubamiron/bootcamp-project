c53453f1ee5fefd6fe7897d81b04a9322f8b41e8
liubamiron <liuba.miron@gmail.com>
2022-01-14 18:47:05 +0200

    indicates mailer in the sending method

--- controllers/ContactUsController.php
+++ controllers/ContactUsController.php
@@ -3,10 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Request\ContactUsRequest;
-// use Illuminate\Http\Request;
-use Illuminate\Http\RedirectResponse;
+use App\Services\ContactUsMailer;
 use Illuminate\Contracts\View\View;
-use Illuminate\Mail\Message;
+use Illuminate\Http\RedirectResponse;
+
 
 class ContactUsController extends Controller
 {
@@ -14,35 +14,16 @@ class ContactUsController extends Controller
         return view('contacts.contactUs');
     }
 
-    public function send(ContactUsRequest $request): RedirectResponse
+
+    public function send(ContactUsRequest $request, ContactUsMailer $mailer): RedirectResponse
     {
-        // dd($request->validated());
-
-        // $callback = function (string $test): string {
-        //      return "{$test} input";
-        //  };
-
 
         $data = $request->validated();
 
-        \Log::debug($callback('test'), $data);
+        
+        \Log::debug('test', $data);
 
-        \Mail::send(
-            'emails.contactUs', 
-            [
-                'email' => $data['email'],
-                'name' => $data['name'],
-                'districts' => $data['districts'],
-                'department' => $data['department'],
-                'messageText' => $data['message'],
-           
-            ],
-            function (Message $message) use ($data) {
-                $message->subject('message from ' . $data['email']);
-                $message->to('rent_online@gmail.com');
-                $message->from('allrent@gmail.com', 'AllRent mailer');
-        }
-        );
+        $mailer->send($data);
 
         return redirect()->route('contactUs')->withInput($data);
         
