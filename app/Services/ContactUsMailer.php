e5dc2d2c97f60e735790c8a48dfe4b5665c4b1d4
liubamiron <liuba.miron@gmail.com>
2022-01-14 18:28:59 +0200

    impliment logic from static \Mail

--- app/Services/ContactUsMailer.php
+++ app/Services/ContactUsMailer.php
@@ -13,4 +13,25 @@ class ContactUsMailer
         $this->infrastructureMailer = $infrastructureMailer;
     }
 
+    public function send(array $data): void
+
+    {
+        $this->infrastructureMailer->send(
+            'emails.contactUs', 
+            [
+                'email' => $data['email'],
+                'name' => $data['name'],
+                'districts' => $data['districts'],
+                'department' => $data['department'],
+                'messageText' => $data['message'],
+           
+            ],
+            function (Message $message) use ($data) {
+                $message->subject('message from ' . $data['email']);
+                $message->to('rent_online@gmail.com');
+                $message->from('allrent@gmail.com', 'AllRent mailer');
+            }
+        );
+    }
+
 }
