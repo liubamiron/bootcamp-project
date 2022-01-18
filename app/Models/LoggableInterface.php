fcbb5406ebced111df3e060516a85496dcb285fa
liubamiron <liuba.miron@gmail.com>
2022-01-18 23:37:16 +0200

    create LoggableInterface

--- app/Models/LoggableInterface.php
+++ app/Models/LoggableInterface.php
@@ -7,7 +7,6 @@ interface LoggableInterface
     // Get Loggable unique name for message generation
     // return string
 
-   
     public function convertToLoggableString():string;
 
     // Get loggable object data for log context
