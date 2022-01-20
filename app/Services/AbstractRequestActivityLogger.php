86f14403a62a23a8c2b41874dd889faa3ef97231
liubamiron <liuba.miron@gmail.com>
2022-01-20 14:51:19 +0200

    create DummyRequestActivityLogger

--- app/Services/AbstractRequestActivityLogger.php
+++ app/Services/AbstractRequestActivityLogger.php
@@ -11,10 +11,10 @@ abstract class AbstractRequestActivityLogger implements RequestActivityLoggerInt
 
     private LoggerInterface $logger;
 
-    public function __construct(LoggerInterface $logger)
-    {
-        $this->logger = $logger;
-    }
+     public function __construct(LoggerInterface $logger)
+     {
+         $this->logger = $logger;
+     }
 
     public function logRequest(Request $request, string $type):void
     {
