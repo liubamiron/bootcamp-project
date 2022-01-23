944b4b3f5644fbefeaef40e17de571e03eeae239
liubamiron <liuba.miron@gmail.com>
2022-01-23 23:27:12 +0200

    impliment RequestActivityLoggerInterface

--- app/Http/Middleware/LogActivityMiddleware.php
+++ app/Http/Middleware/LogActivityMiddleware.php
@@ -2,29 +2,33 @@
 
 namespace App\Http\Middleware;
 
-// use App\Models\User;
-use App\Services\UserReprsenetationTrait;
-use Closue;
+
+
+use Closure;
 use Illuminate\Http\Request;
-use App\Services\DummyRequestActivityLogger;
+use App\Services\RequestActivityLoggerInterface;
 
-class LogActivityMiddleware {
 
-     use UserRepresentationTrait;
+  class LogActivityMiddleware {
 
-    private DummyRequestActivityLogger $logger;
+    private RequestActivityLoggerInterface $logger;
+ 
+    
+    public function __construct(RequestActivityLoggerInterface $logger)
+   {
+       $this->logger = $logger;
+   }
+   // param Request $request
+   // param Closure $next
+   // param string|null $type
+     
+   public function handle($request, Closure $next, ?string $type = null)
+   {
+  
 
-    public function __construct(DummyRequestActivityLogge $logger)
-    {
-        $this->logger = $logger;
-    }
-      
-    public function handle($request, Closure $next, ?string $type = null)
-    {
+        $this->logger->logRequest($request, $type ?? 'unknown page');
+        
+        return $next ($request);
+       }
    
-         $this->logger->logRequest($request, $type ?? 'uknown page');
-
-    return $next ($request);
-    }
-
-}
+   } 
