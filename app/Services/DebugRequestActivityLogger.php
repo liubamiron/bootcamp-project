3ea54b383460b4dc58f86908cf34ea520dd1b052
liubamiron <liuba.miron@gmail.com>
2022-01-26 21:51:43 +0200

    add requests

--- app/Services/DebugRequestActivityLogger.php
+++ app/Services/DebugRequestActivityLogger.php
@@ -3,20 +3,45 @@
 namespace App\Services;
 
 use Illuminate\Http\Request;
+use App\Models\LoggableInterface;
+use App\Models\Imobil;
 
 class DebugRequestActivityLogger extends AbstractRequestActivityLogger
 {
     protected function collectRequestData(Request $request): array
     {
-        return [$request->email,
-        $request->department,
-        $request->name, 
-        $request->type,
-        $request->districts, 
-        $request->department, 
-        $request->message ];
+        // return ['dummy debug data'];
+        // return (array)$request['id'];
+
+        $ipAddress = $request->ip();
+        $token = $request->bearerToken();
+        $urlWithQueryString = $request->fullUrl();
 
 
+        $name = $request->name;
+        $email = $request->email;
+        $department = $request->department;
+        $type = $request->type;
+        $districts = $request->districts; 
+        $department = $request->department;
+        $message = $request->message;
+
+
+        return [
+            'url' . $urlWithQueryString,
+            'name is: ' .$name,
+            'email is' . $email,
+            'ip Address is: ' .$ipAddress,
+            'token is: ' .$token,
+            'department is' . $department,
+            'name is' . $name,
+            'type' . $type,
+            // 'districts' .$districts,
+            'department' .$department,
+            'message' .$message,
+        ];
         
     }
+    
+    
 }
