72bd22fc225ea8ae2a64602fb3d4f74e40d9dbc6
liubamiron <liuba.miron@gmail.com>
2022-01-26 22:18:18 +0200

    impliment logModel2 into ItemController

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -3,23 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Imobil;
+use App\Services\ModelLogger;
 use Illuminate\Http\Request;
-use Psr\Log\LoggerInterface;
 
 class ItemController extends Controller
 {
 
-    public function index($imobilId, Request $request, LoggerInterface $logger)
+    public function index($imobilId, Request $request, ModelLogger $logger)
     {
 
         $imobil = Imobil::findOrFail($imobilId);
 
-        $user = $request->user();
-        $userRepresentation = $user ? "User with id {$user->id}" : "Uknown User";
-        $logger->info(
-            $userRepresentation . ' accesed ' . "imobil with {$imobilId}",
-            ['id' => $imobilId,
-                'price' => $imobil->price], );
+        $logger->logModel2($user = $request->user(), $imobil);
 
         return view('imobils.test', ['imobil' => $imobil]);
     }
