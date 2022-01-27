6704343a2d13c2a1d4439b27202224cec1ffcab9
liubamiron <liuba.miron@gmail.com>
2022-01-27 17:34:57 +0200

    change class, delete comments

--- views/imobils/test.blade.php
+++ views/imobils/test.blade.php
@@ -1,35 +1,22 @@
-         
-          <div class="card shadow-sm">
 
-{{-- <img src="{{ asset('/assets/images/studio.jpg/') }}"  alt="{{ $imobil ->type }}">  --}}
+<div class="card">
 
-<img src="{{ asset('/assets/images/studio.jpg/') }}"> 
+<img src="{{ asset('/assets/images/image' .$imobil->id. '.jpg') }}">
 
-{{-- <h4>  {{$path1}}</h4> --}}
  <div class="card-body">
    <p class="card-text">{{ $imobil->id }}</p>
    <div class="d-flex justify-content-between align-items-center">
      <div class="btn-group">
-
-       {{-- <a href="{{route('itemById', ['id' => $imobil ?? ''->id]) }}"
+      <div class="btn-group">
+        <a href="{{route('itemById', ['id' => $imobil->id]) }}"
        class="btn btn-sm btn-outline-secondary">
        View
-     </a> --}}
-<div class="btn-group">
-     <a href="{{route('itemById', ['id' => $imobil->id]) }}"
-       class="btn btn-sm btn-outline-secondary">
-       View
-     </a>
+      </a>
 
      </div>
-     {{-- <small class="text-muted">{{ $imobil ?? ''->created_at }}</small> --}}
+
    </div>
  </div>
 </div>
 
-
-
-
-
 </div>
-
