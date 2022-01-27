3b514559e7c76d1b74f1dbed9634bae1c39dbc59
liubamiron <liuba.miron@gmail.com>
2022-01-27 19:37:36 +0200

    collected data in a single key

--- app/Services/DebugRequestActivityLogger.php
+++ app/Services/DebugRequestActivityLogger.php
@@ -10,36 +10,8 @@ class DebugRequestActivityLogger extends AbstractRequestActivityLogger
 {
     protected function collectRequestData(Request $request): array
     {
-        // return ['dummy debug data'];
-        // return (array)$request['id'];
-
-        $ipAddress = $request->ip();
-        $token = $request->bearerToken();
-        $urlWithQueryString = $request->fullUrl();
-
-
-        $name = $request->name;
-        $email = $request->email;
-        $department = $request->department;
-        $type = $request->type;
-        $districts = $request->districts; 
-        $department = $request->department;
-        $message = $request->message;
-
-
-        return [
-            'url' . $urlWithQueryString,
-            'name is: ' .$name,
-            'email is' . $email,
-            'ip Address is: ' .$ipAddress,
-            'token is: ' .$token,
-            'department is' . $department,
-            'name is' . $name,
-            'type' . $type,
-            // 'districts' .$districts,
-            'department' .$department,
-            'message' .$message,
-        ];
+        return (array)$request->all(
+            'ip', 'bearerToken', 'fillUrl', 'name','email', 'type', 'districts', 'department','message');
         
     }
     
