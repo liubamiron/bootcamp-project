9af722ff83bb98470376ae2d2407d5dbfcb5be0c
liubamiron <liuba.miron@gmail.com>
2022-01-27 16:52:33 +0200

    delete commented code

--- app/Http/Middleware/LogActivityMiddleware.php
+++ app/Http/Middleware/LogActivityMiddleware.php
@@ -11,24 +11,18 @@ use App\Services\RequestActivityLoggerInterface;
 
   class LogActivityMiddleware {
 
-    private RequestActivityLoggerInterface $logger;
- 
+    private RequestActivityLoggerInterface $logger; 
     
     public function __construct(RequestActivityLoggerInterface $logger)
    {
        $this->logger = $logger;
    }
-   // param Request $request
-   // param Closure $next
-   // param string|null $type
-     
+
    public function handle($request, Closure $next, ?string $type = null)
    {
-  
-
         $this->logger->logRequest($request, $type ?? 'unknown page');
         
         return $next ($request);
-       }
+    }
    
-   } 
+  } 
