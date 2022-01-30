a61787be9eee4b34c66cccd279cc00807a31fe7c
liubamiron <liuba.miron@gmail.com>
2022-01-30 19:20:29 +0200

    russian roulette, add random nr. statistics

--- app/Console/Commands/Game.php
+++ app/Console/Commands/Game.php
@@ -13,14 +13,14 @@ class Game extends Command
      *
      * @var string
      */
-    protected $signature = 'game:test';
+    protected $signature = 'game1';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'russian roulette game';
 
     private CacheRepository $cacheRepository;
 
@@ -47,57 +47,87 @@ class Game extends Command
     public function handle()
     {
 
-        $lastVisit = $this->cacheRepository->get('lastCommandVisit', 'never');
+        $lastVisit = $this->cacheRepository->get('lastGameVisit', 'never');
         $diedStats = $this->cacheRepository->get('diedCount', 0);
         $surviveStats = $this->cacheRepository->get('surviveCount', 0);
 
 
-        $this->info("Command called last time: {$lastVisit}");
-        $this->info("Died Statistic: {$diedStats}");
-        $this->info("Survive Statistic: {$surviveStats}");
+        $this->cacheRepository->set('lastGameVisit', date(\DateTime::ATOM),);
+        
+        $this->info("Game visit last time: {$lastVisit}");
 
-        $this->cacheRepository->set('lastCommandVisit', date(\DateTime::ATOM), 86400);
+        $bad_numbers = $this->cacheRepository->get('Bad-numbers', []);
+
+        
 
 
         $executionCicle = True;
         $count = 1;
         $table = [];
+        $table2 = [];
         $answer = 'survive';
         $res = 0;
 
         while ($executionCicle) {
-            $random = $random = random_int(0, 2);
+
+            $random = $random = random_int(1, 6);
+
+            
+            $bad_numbers[$random] = $bad_numbers[$random] ?? 0;
+            $bad_numbers[$random]++;
+    
+            $this->cacheRepository->set('Bad-numbers', $bad_numbers, 86400);
+    
+
+           
             $res = $this->ask("Pull the triger!");
 
             if ($random === 1) {
                 $answer = 'die';
-                $table[] = [$answer, $count];
-                $this->table(['answer', 'count'], $table);
+                $table[] = [$answer, $count, $random];
+                $this->table(['answer', 'attempts', 'random_nr'], $table);
+                
+                
 
                 $executionCicle = False;
                 $this->info("You died");
 
                 $diedStats++;
+
                 $this->cacheRepository->set('diedCount', $diedStats, 86400);
+              
 
             } elseif ($count > 6) {
                 $executionCicle = False;
                 $this->info("You survived");
 
                 $surviveStats++;
+                
                 $this->cacheRepository->set('surviveCount', $surviveStats, 86400);
+                
 
             } else {
-                $table[] = [$answer, $count];
-                $this->table(['answer', 'count'], $table);
+                $table[] = [$answer, $count, $random];
+                $this->table(['answer', 'attempts', 'random_nr'], $table);
 
                 $count++;
                 $this->info("You survived");
 
             }
+        } 
+
+
+        $this->info("Died Statistic: {$diedStats}");
+        $this->info("Survive Statistic: {$surviveStats}");
+
+        $this->info("Number how many times was randomized, statistics :");
+
+        foreach ($bad_numbers as $key => $count) {
+            $table2[] = [$key, $count];
         }
+        $this->table(['random nr.', 'count'], $table2);
 
-
+        
     }
-
+    
 }
